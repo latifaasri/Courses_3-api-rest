8b0197a0660c12d4457b11ca54d57cc6fda2f5de
houriaasri <houria1asri@gmail.com>
2021-10-04 15:41:44 +0200

    Added POST,PUT, PATCHand DELETE

--- profiles_projects/profiles_api/urls.py
+++ profiles_projects/profiles_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from profiles_api import views
 
 urlpatterns = [
-    path('', views.index)
+    path('hello-view', views.HelloApiView.as_view()),
 ]
