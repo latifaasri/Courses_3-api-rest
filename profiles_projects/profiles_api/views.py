7240dcff2022c9e7dbbb35af79baf881b46ffd45
houriaasri <houria1asri@gmail.com>
2021-10-07 15:45:26 +0200

    Modification token

--- profiles_projects/profiles_api/views.py
+++ profiles_projects/profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
--- profiles_projects/profiles_projects/settings.py
+++ profiles_projects/profiles_projects/settings.py
@@ -39,9 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'profiles_api.apps.ProfilesApiConfig',
     'rest_framework',
+    'rest_framework.authtoken',
 
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
